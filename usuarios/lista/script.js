c333531a901854eb985bc92918b36ca24bd98e24
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-10 14:31:38 -0300

    add: search by user
    Adicionado a pesquisa por usuário específico, através do parâmetro (id_usuario).

--- usuarios/lista/script.js
+++ usuarios/lista/script.js
@@ -1,10 +1,42 @@
 var list = [];
 
+var list2 = [];
+
+const createRowsEspecific = () => {
+    list2.forEach(data => {
+        row = `
+        <thead>
+            <tr>
+                <th>Nome:</th>
+                <th>E-mail:</th>
+                <th>Cargo:</th>
+                <th>CPF</th> 
+                <th>Aniversario</th>
+                <th>Nacionalidade</th>
+                <th></th>
+            </tr>
+         </thead>
+            <tr>
+                <td> ${ data.nome } </td>
+                <td> ${ data.email } </td>
+                <td> ${ data.cargo } </td>
+                <td> ${ data.cpf } </td>
+                <td> ${ data.dataNascimento } </td>
+                <td> ${ data.nacionalidade } </td>
+                <td> <button onclick="deleteUser(${ data.id_usuario })">Excluir Usuario</button></td>
+                <td><a href="${data.link}">Editar Usuario</a></td>
+            </tr>
+        `
+        $("#table_especific tr:last").after(row) ;
+    })
+}
+
 const createRows = () => {
     list.forEach(data => {
         row = `
             <tr>
                 <td> ${ data.id_usuario } </td>
+                <td> ${ data.nome } </td>
                 <td> ${ data.cargo } </td>
                 <td> ${ data.cpf } </td>
                 <td> ${ data.dataNascimento } </td>
@@ -45,4 +77,33 @@ const deleteUser = (id) => {
     $.ajax(request);
 }
 
-getUsers()
+const getEspecificUser = (id) => {
+    const request = {
+        type:'GET',
+        url: `${URL_API}user/${id}`,
+        success: function(response) {
+            list2 = [response.data];
+            createRowsEspecific()
+        }
+    };
+
+    $.ajax(request);
+}
+
+
+getUsers();
+
+function teste(){
+
+    clearScreen();
+
+    var input = document.getElementById("idUser").value;
+
+    alert(input);
+
+    getEspecificUser(input);
+}
+
+function clearScreen(){
+    $('#table_especific').html(""); //Limpando o resultado
+}
