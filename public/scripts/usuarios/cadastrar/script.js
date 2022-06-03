7ef64d1d49e2fef6964439d74077f5ccb41495d0
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-03 14:17:52 -0300

    update req cadastro

--- public/scripts/usuarios/cadastrar/script.js
+++ public/scripts/usuarios/cadastrar/script.js
@@ -2,19 +2,24 @@ const createUser = () => {
     const formValues = document.forms.cadastrarUsuario
     const payload = {
         nome: formValues.nome.value,
+        email: formValues.email.value,
         dataNascimento: formValues.dataNascimento.value,
         cpf: formValues.cpf.value,
         nacionalidade: formValues.nacionalidade.value,
+        cargo: formValues.cargo.value,
         senha: formValues.senha.value
     }
+    
     const request = {
         type:'POST',
         url:`${URL_API}user`,
-        dataType: "json",
-        data: payload,
+        dataType: 'json',
+        contentType: 'application/json',
         success: function(response) {
             alert(response.message)
-        }
+        },
+        data: JSON.stringify(payload),
     };
+    
     $.ajax(request);
 }
