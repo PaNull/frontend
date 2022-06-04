ebd7d1ad1554bddb81b064d7da383eb9eb37613e
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-04 15:55:14 -0300

    add: cadastro de campeonatos via API

--- public/scripts/campeonatos/cadastrar/script.js
+++ public/scripts/campeonatos/cadastrar/script.js
@@ -1,20 +1,64 @@
-const createTournament = () => {
-    const formValues = document.forms.cadastrarCampeonato
-    const payload = {
-        nome: formValues.nome.value,
-        modalidade: formValues.modalidade.value,
-        premiacao: formValues.premiacao.value,
-        dataCampeonato: formValues.dataCampeonato.value,
-        qtdTimes: formValues.qtdTimes.value,
-    }
-    const request = {
-        type:'POST',
-        url:`${URL_API}tournament`,
-        dataType: "json",
-        data: payload,
-        success: function(response) {
-            alert(response.message)
+function cadastrarCamp() {
+
+    let nome = document.getElementsByName("nome")[0].value
+    let modalidade = document.getElementsByName("modalidade")[0].value
+    let premiacao = document.getElementsByName("premiacao")[0].value
+    let dataStartCamp = document.getElementsByName("dataStartCamp")[0].value
+    let dataEndCamp = document.getElementsByName("dataEndCamp")[0].value
+    let qtdTimes = document.getElementsByName("qtdTimes")[0].value
+    
+    let elementos = [nome, modalidade, premiacao, dataStartCamp, dataEndCamp, qtdTimes];
+    
+    var campos = document.getElementsByTagName("input");
+   
+   
+    for(var i = 0; i < elementos.length-1; i++){
+        if(i==2){
+            if(modalidade == "disabled"){
+                alert("Escolha uma modalidade!");
+                campos[i].focus();
+                return
+            }
         }
+        if(campos[i].value == ""){
+            alert("Preencha o campo " + campos[i].name + "!" );
+            campos[i].focus();
+            return;
+        }
+    }
+
+    const payload = {
+        nome: nome,
+        modalidade: modalidade,
+        dataStartCampeonato: dataStartCamp,
+        dataEndCampeonato: dataEndCamp,
+        premiacao: premiacao,
+        qntdTimes: qtdTimes
+    }
+
+    console.log("chegou no create")
+
+   //FETCH POST
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
     };
-    $.ajax(request);
+
+    fetch(`${URL_API}tournament`, options)
+        .then(data => {
+            if (!data.ok) {
+                throw Error(data.status);
+            }
+            return data.json();
+
+        }).then(payload => {
+            console.log(payload);
+        }).catch(e => {
+            console.log(e);
+        }); 
 }
+
+console.log("script loaded")
