3d522e7f04482bf22941ae8e6ece9d84b6e00065
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-04 16:25:57 -0300

    add: consulta de campeonatos via API

--- public/scripts/campeonatos/lista/script.js
+++ public/scripts/campeonatos/lista/script.js
@@ -1,14 +1,39 @@
 var list = [];
 
+var list2= [];
+
+const createRowsEspecific = () => {
+    document.getElementById("table_especific").style.display = 'table';
+    list2.forEach(data => {
+        row = `
+            <tr>
+                <td> ${ data.nome } </td>
+                <td> ${ data.premiacao } </td>
+                <td> ${ data.modalidade } </td>
+                <td> ${ data.qntdTimes } </td>
+                <td> ${ data.dataStartCampeonato } </td>
+                <td> ${ data.dataEndCampeonato } </td>
+                <td> ${ data.etapa } </td>
+                <td> <button onclick="deleteUser(${ data.id_usuario })" class="btnExcluir">Excluir Usuario</button></td>
+                <td><a href="${data.link}" class="edtUser">Editar Usuario</a></td>
+            </tr> 
+        `
+        $("#table_especific tbody").append(row);
+    })
+}
+
 const createRows = () => {
     list.forEach(data => {
         row = `
             <tr>
+                <td> ${ data.id_campeonato } </td>
                 <td> ${ data.nome } </td>
-                <td> ${ data.modalidade } </td>
                 <td> ${ data.premiacao } </td>
-                <td> ${ data.dataCampeonato } </td>
-                <td> ${ data.qtdTimes } </td>
+                <td> ${ data.modalidade } </td>
+                <td> ${ data.qntdTimes } </td>
+                <td> ${ data.dataStartCampeonato } </td>
+                <td> ${ data.dataEndCampeonato } </td>
+                <td> ${ data.etapa } </td>
                 <td> <button class="btnDeletar" onclick="deleteTournament(${ data.id_campeonato })">Excluir Campeonato</button></td>
                 <td><a href="${data.link}" class="editarCamp">Editar Campeonato</a></td>
             </tr>
@@ -17,15 +42,6 @@ const createRows = () => {
     })
 }
 
-            <tr>
-                <td> ${ data.nome } </td>
-                <td> ${ data.modalidade } </td>
-                <td> ${ data.premiacao } </td>
-                <td> ${ data.dataCampeonato } </td>
-                <td> ${ data.qtdTimes } </td>
-                <td> <button class="btnDeletar" onclick="deleteTournament(${ data.id_campeonato })">Excluir Campeonato</button></td>
-                <td><a href="${data.link}" class="editarCamp">Editar Campeonato</a></td>
-            </tr>
 
 const getTournaments = () => {
     const request = {
@@ -55,5 +71,35 @@ const deleteTournament = (id) => {
     $.ajax(request);
 }
 
+const getEspecificTournament = (id) => {
+    const request = {
+        type:'GET',
+        url: `${URL_API}tournament/${id}`,
+        success: function({ data }) {
+            list2 = [data];
+            createRowsEspecific()
+        },
+        error: () => {
+            document.getElementById("table_especific").style.display = 'none';
+            alert('Erro')
+        }
+    };
+
+    $.ajax(request);
+}
 
 getTournaments()
+
+function tournamentEspecific(){
+    var input = document.getElementById("idTournament").value;
+
+    clearScreen();
+    getEspecificTournament(input);
+}
+
+function clearScreen(){
+    if (list2.length) {
+        document.querySelector("#table_especific tbody").deleteRow(0);
+        list2 = []
+    }
+}
